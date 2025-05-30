b177952be4fbca9f24af9539d71dd76cd4728c8c
GianlucaRizzo1807 <n01491807@humber.ca>
2025-05-29 21:50:47 -0400

    Initial commit
    Added existing portfolio to repo

---
+++ js/clipboard.js
@@ -0,0 +1,8 @@
+function CpySchoolEmail() {
+  var copyText = "n01491807@humber.ca";
+  navigator.clipboard.writeText(copyText);
+}
+function CpyPersonalEmail() {
+  var copyText = "gianlucarizzo440@gmail.com";
+  navigator.clipboard.writeText(copyText);
+}
