3bcbee8c780db173dd90e78c9275e6270e8fd573
GianlucaRizzo <121537559+grizz142003@users.noreply.github.com>
2025-06-03 18:51:34 -0400

    tried implimenting carousel

---
+++ js/carousel.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const carousel = document.querySelector('.carousel-inner');
+  if (!carousel) return;
+
+  const cards = Array.from(carousel.children);
+  const angleStep = 360 / cards.length;
+  const radius = carousel.clientWidth / 2;
+  let angle = 0;
+
+  cards.forEach((card, idx) => {
+    card.style.transform = `rotateY(${idx * angleStep}deg) translateZ(${radius}px)`;
+  });
+
+  function update() {
+    carousel.style.transform = `translateZ(-${radius}px) rotateY(${angle}deg)`;
+  }
+
+  document.querySelector('.next').addEventListener('click', () => {
+    angle -= angleStep;
+    update();
+  });
+
+  document.querySelector('.prev').addEventListener('click', () => {
+    angle += angleStep;
+    update();
+  });
+
+  update();
+});
